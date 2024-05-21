3eeed560b2f2e8fb665e98ae9bc795d36f05df5b
nikofuertez <152659262+nikofuertez@users.noreply.github.com>
2024-05-20 18:04:32 -0700

    Update asg5b.js

--- ASG5A/asg5b.js
+++ ASG5A/asg5b.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { TextureLoader } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
